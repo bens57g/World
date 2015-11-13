7764b30db51f31ebf4799509b41c3214dfab6e20
bens57g <bens.57@hotmail.fr>
2015-11-13 15:32:57 +0100

    modifications pour div gauche

--- functions.php
+++ functions.php
@@ -1,4 +1,36 @@
 <?php 
-	// regroupement des fonction
+	// regroupement des fonctions
+
+	// fonction qui récupère le nom de tous les continents
+	function getContinent($pdo){
+		$sql = 'SELECT Continent FROM country GROUP BY Continent';
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute();
+		$row = $stmt->fetchAll();
+		return $row;
+	}
+
+	// fontion qui récupère le nombre de pays pour un continent choisi
+	function NbrePays($continent, $pdo){
+		$continent = $_GET['continent'];
+		$sql = 'SELECT COUNT(*) AS nb FROM country WHERE Continent ="' .$continent .'";';
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute();
+		$row = $stmt->fetch();
+		return $row['nb'];
+	}
+
+	function getPopulation($continent, $pdo){
+		$continent = $_GET['continent'];
+		$sql = 'SELECT SUM(Population) AS NbPop FROM country WHERE Continent ="'.$continent.'";';
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute();
+		$row = $stmt->fetchAll();
+		return $row;
+	}
+
+
+
+
 
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php  
+	// connexion à la base + debug
+	$dsn = 'mysql:host=localhost;dbname=world;charset=utf8';
+	$pdo = new PDO($dsn, 'root', 'webforce3');
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	require 'functions.php';
+	
+	// nom des continents
+	$Continent_names = getContinent($pdo);
 
 
 ?>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,7 +24,6 @@
 	<link rel="stylesheet" href="style.css">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-	<script src="pigeons.js" type="text/javascript" charset="utf-8" async defer></script>
 </head>
 <body>
 	<main>
@@ -21,9 +32,16 @@
 			<h1>Monde</h1> 
 		</div>
 		<!--les deux select -->
-		<select class="continent">
-			<option value="">Continent</option>
-		</select>
+		<form action="index.php" method="GET" accept-charset="utf-8">
+			<select class="continent" name="contient">
+			<?php // affichage des options de continent
+				foreach ($Continent_names as $key) {
+					echo '<option value="'. $key['Continent'] . '">' .$key['Continent']. '</option>';
+				}
+			?>
+			</select>
+			<input class="button_left" type="submit" name="submit_left" value="Envoyer" />
+		</form>
 
 		<select class="pays" name="pays" >
 			<option value="">Pays</option>
@@ -33,6 +51,16 @@
 		
 		<div class="div_left">
 			<h1>Continent Démographie </h1>
+			<?php 
+				// nbre de pays
+				if(isset($_GET['contient'])) {
+					$nb_pays = NbrePays($continent, $pdo);
+					echo 'Le nombre de pays : ' . $nb_pays .'<br>';
+					// population
+					$Total_population = getPopulation($continent, $pdo);
+					echo 'Population totale : ' . $Total_population .'<br>';
+				}
+			?>
 		</div>
 
 		<div class="div_right">
